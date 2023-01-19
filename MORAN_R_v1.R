d7f581932154694f862b43436c8dec974568bb4a
israelaod <israelaod@gmail.com>
2023-01-19 13:38:48 -0600

    se sueben capas y archivos de los mapas HTML

--- MORAN_R_v1.R
+++ MORAN_R_v1.R
@@ -43,8 +43,13 @@ per$BI_LISA<-ifelse(per$LISA_CL=="0", "NO SIGNIFICATIVO",
                        ifelse(per$LISA_CL=="2","BAJO-BAJO", 
                               ifelse(per$LISA_CL=="3","BAJO-ALTO", 
                                      ifelse(per$LISA_CL=="4","ALTO-BAJO","SIN VECINOS")))))
-
-
+#creamos poligonos separados segun los rangos 
+no_signi<-subset(per, per$BI_LISA=="NO SIGNIFICATIVO")
+alto_alto<-subset(per, per$BI_LISA=="ALTO-ALTO")
+bajo_bajo<-subset(per, per$BI_LISA=="BAJO-BAJO")
+bajo_alto<-subset(per, per$BI_LISA=="BAJO-ALTO")
+alto_bajo<-subset(per, per$BI_LISA=="ALTO-BAJO")
+sin_vecinos<-subset(per, per$BI_LISA=="SIN VECINOS")
 
 # creamos una paleta de colores personalizada 
 factor(per$BI_LISA)
@@ -95,7 +100,7 @@ labels <- c("<b>Mapa de cluster análisis de bivariado de Moran</b>",
 )
 sizes <- c(0,10,10,10,10,10,10)
 shapes <- c("circle","square","square","square","square","square","square")
-borders <- c("transparent","red", "darkblue", "lightblue", "pink", "gray65","gray")
+borders <- c("transparent","red", "darkblue", "lightblue", "pink", "#E5E5E5","gray")
 
 addLegendCustom <- function(map, colors, labels, sizes, shapes, borders, opacity = 0.5){
   
@@ -116,9 +121,9 @@ addLegendCustom <- function(map, colors, labels, sizes, shapes, borders, opacity
   return(addLegend(map, colors = legend_colors, labels = legend_labels, opacity = opacity,position = "bottomleft"))
 }
 
-#rr <- tags$div(
-# HTML('<a href="https://cran.r-project.org/"> <img border="0" alt="ImageTitle" top= "-50px" src="https://i.ibb.co/544jKfq/tabla2.jpg" width="424" height="345"> </a>')
-#)  
+rr <- tags$div(
+HTML('<a href="https://i.ibb.co/WDhhdYk/AGEB-lisa-value-Lisa-Scatter-Plot-Frame.png" target="_blank"> <img border="0" alt="ImageTitle" top= "-50px" src="https://i.ibb.co/WDhhdYk/AGEB-lisa-value-Lisa-Scatter-Plot-Frame.png" width="300" height="150"> </a>')
+)
 
 
 #GENERAMOS MAPA--------------------------------------------------------------------------------------------------------
@@ -127,7 +132,7 @@ leaflet()  %>%addTiles() %>%addProviderTiles(providers$Esri.WorldGrayCanvas) %>%
   
   addFullscreenControl()%>%clearBounds()%>%
   addControl(title,position = "topleft",className="map-title")%>%
-  #  addControl(rr,position = "bottomleft")%>% #TABLA DE DATOS
+  addControl(rr,position = "bottomleft")%>% #TABLA DE DATOS
   leafem::addMouseCoordinates() %>% addFullscreenControl(position = "topleft", pseudoFullscreen = F) %>%
   leafem::addLogo(img=ciudad$city,src="remote",position="bottomright",width=403,height=127)%>%
   #  addDrawToolbar(editOptions = editToolbarOptions(selectedPathOptions()),polylineOptions = drawPolylineOptions(metric = T),
@@ -140,21 +145,63 @@ leaflet()  %>%addTiles() %>%addProviderTiles(providers$Esri.WorldGrayCanvas) %>%
   addMapPane("li",zIndex=570)%>%
   #addMapPane("col",zIndex = 550)%>%
   addMapPane("lo",zIndex = 580)%>%
-  addPolygons(data=per ,color = "#7F7F7F",fillColor = "transparent",fillOpacity =0.01,weight = 0.5,
-              highlightOptions = highlightOptions(color = "#7F7F7F", weight = 0.5) , group="AGEB",options = pathOptions(pane="polygons"))%>%
+  # addPolygons(data=per ,color = "#7F7F7F",fillColor = "transparent",fillOpacity =0.01,weight = 0.5,
+  #             highlightOptions = highlightOptions(color = "#7F7F7F", weight = 0.5) , group="AGEB",options = pathOptions(pane="polygons"))%>%
+  # 
   
-  
-  addPolygons(data=per,
-              stroke = FALSE, 
-              fillColor = pal_fun(per$LISA_CL),
+ addPolygons(data=alto_alto,
+             color =  "gray" ,weight = 0.5 ,highlightOptions = highlightOptions(color = "gray", weight = 0.5) , 
+              fillColor = pal_fun(alto_alto$LISA_CL),
               fillOpacity = 0.8, smoothFactor = 0.5,
-              popup = paste("<b>","AGEB : ","</b>",per$CVE_AGEB,"<br>",
-                            "<b>","valor de correlacion : ","</b>",per$BI_LISA,"<br>"),group="AGEB",options = pathOptions(pane="polygons")) %>%
+              popup = paste("<b>","AGEB : ","</b>",alto_alto$CVE_AGEB,"<br>",
+                            "<b>","VALOR DE CORRELACION : ","</b>",alto_alto$BI_LISA,"<br>",
+                            "<b>","HOMICIDIOS: ","</b>",alto_alto$HOMICIDIOS,"<br>",
+                            "<b>","NARCOMENUDEO : ","</b>",alto_alto$NARCOMENUD),group="ALTO-ALTO",options = pathOptions(pane="polygons")) %>%
+  addPolygons(data=bajo_bajo,
+              color =  "gray" ,weight = 0.5 ,highlightOptions = highlightOptions(color = "gray", weight = 0.5) , 
+              fillColor = pal_fun(bajo_bajo$LISA_CL),
+              fillOpacity = 0.8, smoothFactor = 0.5,
+              popup = paste("<b>","AGEB : ","</b>",bajo_bajo$CVE_AGEB,"<br>",
+                            "<b>","valor de correlacion : ","</b>",bajo_bajo$BI_LISA,"<br>",
+                            "<b>","HOMICIDIOS: ","</b>",bajo_bajo$HOMICIDIOS,"<br>",
+                            "<b>","NARCOMENUDEO : ","</b>",bajo_bajo$NARCOMENUD),group="BAJO-BAJO",options = pathOptions(pane="polygons")) %>%
+    addPolygons(data=bajo_alto,
+                color =  "gray" ,weight = 0.5 ,highlightOptions = highlightOptions(color = "gray", weight = 0.5) ,
+              fillColor = pal_fun(bajo_alto$LISA_CL),
+              fillOpacity = 0.8, smoothFactor = 0.5,
+              popup = paste("<b>","AGEB : ","</b>",bajo_alto$CVE_AGEB,"<br>",
+                            "<b>","valor de correlacion : ","</b>",bajo_alto$BI_LISA,"<br>",
+                            "<b>","HOMICIDIOS: ","</b>",bajo_alto$HOMICIDIOS,"<br>",
+                            "<b>","NARCOMENUDEO : ","</b>",bajo_alto$NARCOMENUD),group="BAJO-ALTO",options = pathOptions(pane="polygons")) %>%
+      addPolygons(data=alto_bajo,
+                  color =  "gray" ,weight = 0.5 ,highlightOptions = highlightOptions(color = "gray", weight = 0.5) ,
+              fillColor = pal_fun(alto_bajo$LISA_CL),
+              fillOpacity = 0.8, smoothFactor = 0.5,
+              popup = paste("<b>","AGEB : ","</b>",alto_bajo$CVE_AGEB,"<br>",
+                            "<b>","valor de correlacion : ","</b>",alto_bajo$BI_LISA,"<br>",
+                            "<b>","HOMICIDIOS: ","</b>",alto_bajo$HOMICIDIOS,"<br>",
+                            "<b>","NARCOMENUDEO : ","</b>",alto_bajo$NARCOMENUD),group="ALTO-BAJO",options = pathOptions(pane="polygons")) %>%
+        addPolygons(data=no_signi, 
+                    color =  "gray" ,weight = 0.5 ,highlightOptions = highlightOptions(color = "gray", weight = 0.5) ,
+              fillColor = pal_fun(no_signi$LISA_CL),
+              fillOpacity = 0.8, smoothFactor = 0.5,
+              popup = paste("<b>","AGEB : ","</b>",no_signi$CVE_AGEB,"<br>",
+                            "<b>","valor de correlacion : ","</b>",no_signi$BI_LISA,"<br>",
+                            "<b>","HOMICIDIOS: ","</b>",no_signi$HOMICIDIOS,"<br>",
+                            "<b>","NARCOMENUDEO : ","</b>",no_signi$NARCOMENUD),group="NO SIGNIFICATIVO",options = pathOptions(pane="polygons")) %>%
+          addPolygons(data=sin_vecinos,
+                      color =  "gray" ,weight = 0.5 ,highlightOptions = highlightOptions(color = "gray", weight = 0.5) ,
+              fillColor = pal_fun(sin_vecinos$LISA_CL),
+              fillOpacity = 0.8, smoothFactor = 0.5,
+              popup = paste("<b>","AGEB : ","</b>",sin_vecinos$CVE_AGEB,"<br>",
+                            "<b>","valor de correlacion : ","</b>",sin_vecinos$BI_LISA,"<br>",
+                            "<b>","HOMICIDIOS: ","</b>",sin_vecinos$HOMICIDIOS,"<br>",
+                            "<b>","NARCOMENUDEO : ","</b>",sin_vecinos$NARCOMENUD),group="SIN VECINOS",options = pathOptions(pane="polygons")) %>%
   
     
   #ETIQUETAS----------------------------------------------------------
 addLayersControl(overlayGroups = c( "&nbsp; <b>CAPAS</b> &nbsp; ",
-                                    "AGEB"
+                                    "ALTO-ALTO","BAJO-BAJO","BAJO-ALTO","ALTO-BAJO","NO SIGNIFICATIVO","SIN VECINOS"
                                     
                                     
 ),
@@ -188,7 +235,7 @@ options = layersControlOptions(collapsed = T))%>%
   #addLegend(position = "bottomleft",labels = c("Menos de 250 personas","250 a 599 personas","600 a 1000 personas","Más de 1000 personas"),colors = c("#FFFF00","#FFFF00","#FF0000","#FF0000"),title = "AFLUENCIA CENTRO HISTÓRICO")%>%
   
   hideGroup(c( "&nbsp; <b>STVS C5</b> &nbsp; ",
-               "AGEB",
+               "ALTO-ALTO","BAJO-BAJO","BAJO-ALTO","ALTO-BAJO","NO SIGNIFICATIVO","SIN VECINOS",
                
                "&nbsp; <b>MI C911E</b> &nbsp; ",
                "TOT","CONS"))
